12ad96179f53e0cdf098db836d96a2111b17a217
Cristian Araujo <cristian.jesus@zup.com.br>
2020-06-24 20:56:50 -0300

    Conectando a API do github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,59 +1,65 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  // Você não precisa tipar tudo
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories((prevRepositories) => [...prevRepositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
-        <button type="button">Pesquisar</button>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
       </Form>
       <Repositories>
-        <a href="#teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/19674933?s=460&u=25605340bd05f5fa54f2ac70b316623798e5ddf9&v=4"
-            alt="Cristian Araujo"
-          />
-          <div>
-            <strong>araujocristian/github-explores</strong>
-            <p>Uma aplicação para explorar repositórios do Github</p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="#teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="#teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/19674933?s=460&u=25605340bd05f5fa54f2ac70b316623798e5ddf9&v=4"
-            alt="Cristian Araujo"
-          />
-          <div>
-            <strong>araujocristian/github-explores</strong>
-            <p>Uma aplicação para explorar repositórios do Github</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="#teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/19674933?s=460&u=25605340bd05f5fa54f2ac70b316623798e5ddf9&v=4"
-            alt="Cristian Araujo"
-          />
-          <div>
-            <strong>araujocristian/github-explores</strong>
-            <p>Uma aplicação para explorar repositórios do Github</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
